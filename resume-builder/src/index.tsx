9e4972ae2fc5de6f9024a30d4f3043d3d62abf2a
rahulkr2511 <krahulrajank@gmail.com>
2025-05-13 17:56:39 +0530

    comments added

--- resume-builder/src/index.tsx
+++ resume-builder/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Define the root element for React
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
